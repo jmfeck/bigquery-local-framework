663057d758e3ee2c5ba58bf1bd02c0776feeffbe
Joao Manoel Feck <joaomfeck@gmail.com>
2024-11-05 12:28:30 +0100

    Update sample_execute_pipeline.bat
    small correction on the yaml for the config query extractor

--- sample_execute_pipeline.bat
+++ sample_execute_pipeline.bat
@@ -1,5 +1,5 @@
 python scripts/bigquery_ingest_excel.py config/sample_config_ingest.yaml
 python scripts/bigquery_query_trigger.py config/sample_config_query.yaml queries/sample_query.sql
 python scripts/bigquery_table_extractor.py config/sample_config_table_extractor.yaml
-python scripts/bigquery_query_extractor.py config/sample_config_table_extractor.yaml queries/sample_query.sql
+python scripts/bigquery_query_extractor.py config/sample_config_query_extractor.yaml queries/sample_query.sql
 PAUSE
