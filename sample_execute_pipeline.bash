8595b7b38882deddb4853c6c83dad254ec7445c2
Joao Manoel Feck <joaomfeck@gmail.com>
2024-11-05 12:29:14 +0100

    Update sample_execute_pipeline.bash
    adding the query extractor row

--- sample_execute_pipeline.bash
+++ sample_execute_pipeline.bash
@@ -3,7 +3,8 @@
 # Run the Python scripts with the YAMLs config files as an argument
 python scripts/bigquery_ingest_excel.py config/sample_config_ingest.yaml
 python scripts/bigquery_query_trigger.py config/sample_config_query.yaml queries/sample_query.sql
-python scripts/bigquery_table_extractor.py config/sample_config_extractor.yaml
+python scripts/bigquery_table_extractor.py config/sample_config_table_extractor.yaml
+python scripts/bigquery_query_extractor.py config/sample_config_query_extractor.yaml queries/sample_query.sql
 
 # Pause to keep the terminal open after execution
 read -p "Press Enter to continue..."
